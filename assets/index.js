4d70f33d40103289622decceef177aeecf97c885
JJay <iamjj112100@gmail.com>
2022-04-26 23:02:41 +0800

    ok

--- assets/index.js
+++ assets/index.js
@@ -15,61 +15,61 @@ window.onload = function () {
         },
         {
             id: "F04",
-            name: "頂級羊排",
-            price: 330
+            name: "蘆薈佐鮮蝦",
+            price: 120
         },
         {
             id: "F05",
-            name: "義大利麵",
-            price: 180
+            name: "沙拉拼盤",
+            price: 110
         }, {
             id: "F06",
-            name: "夏威夷披薩",
-            price: 220
+            name: "日式炸物",
+            price: 100
         }, {
             id: "F07",
-            name: "頂級羊排",
-            price: 330
+            name: "經典早午餐",
+            price: 230
         },
         {
             id: "F08",
-            name: "義大利麵",
-            price: 180
+            name: "酥炸脆薯",
+            price: 120
         }, {
             id: "F09",
-            name: "夏威夷披薩",
-            price: 220
+            name: "主廚濃湯",
+            price: 90
         }],
         "drink": [
             {
                 id: "D01",
-                name: "頂級羊排",
-                price: 330
+                name: "卡布奇諾",
+                price: 180
             },
             {
                 id: "D02",
-                name: "頂級羊排",
-                price: 330
+                name: "水果茶",
+                price: 150
             },
             {
                 id: "D03",
-                name: "頂級羊排",
-                price: 330
+                name: "雞尾酒",
+                price: 350
             },
             {
                 id: "D04",
-                name: "頂級羊排",
-                price: 330
+                name: "覆盆子鮮奶",
+                price: 150
             },
             {
                 id: "D05",
-                name: "頂級羊排",
+                name: "氣泡水",
                 price: 330
             },
             {
                 id: "D06",
-                name: "頂級羊排",
-                price: 330
+                name: "冰咖啡",
+                price: 60
             }
         ],
         "sweets": [

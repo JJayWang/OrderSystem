09e90e988882074d53d92ffa7e0951fcc98392c6
JJay <iamjj112100@gmail.com>
2022-04-26 23:04:44 +0800

    release

---
+++ assets/menudata.js
@@ -0,0 +1,91 @@
+let meatItems = {
+    "food": [{
+        id: "F01",
+        name: "義大利麵",
+        price: 180
+    }, {
+        id: "F02",
+        name: "夏威夷披薩",
+        price: 220
+    }, {
+        id: "F03",
+        name: "頂級羊排",
+        price: 330
+    },
+    {
+        id: "F04",
+        name: "蘆薈佐鮮蝦",
+        price: 120
+    },
+    {
+        id: "F05",
+        name: "沙拉拼盤",
+        price: 110
+    }, {
+        id: "F06",
+        name: "日式炸物",
+        price: 100
+    }, {
+        id: "F07",
+        name: "經典早午餐",
+        price: 230
+    },
+    {
+        id: "F08",
+        name: "酥炸脆薯",
+        price: 120
+    }, {
+        id: "F09",
+        name: "主廚濃湯",
+        price: 90
+    }],
+    "drink": [
+        {
+            id: "D01",
+            name: "卡布奇諾",
+            price: 180
+        },
+        {
+            id: "D02",
+            name: "水果茶",
+            price: 150
+        },
+        {
+            id: "D03",
+            name: "雞尾酒",
+            price: 350
+        },
+        {
+            id: "D04",
+            name: "覆盆子鮮奶",
+            price: 150
+        },
+        {
+            id: "D05",
+            name: "氣泡水",
+            price: 330
+        },
+        {
+            id: "D06",
+            name: "冰咖啡",
+            price: 60
+        }
+    ],
+    "sweets": [
+        {
+            id: "S01",
+            name: "頂級羊排",
+            price: 330
+        },
+        {
+            id: "S02",
+            name: "頂級羊排",
+            price: 330
+        },
+        {
+            id: "S03",
+            name: "頂級羊排",
+            price: 330
+        }
+    ]
+};
